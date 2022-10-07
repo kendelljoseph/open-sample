898ac2a3a57dd33e69394d6ac08531ecfc5a139f
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-07 15:15:23 -0400

    Simplifies model files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,64 @@
-const Entity = require('./entity') ;
-const Authz = require('./authz');
-const Audit = require('./audit');
-const RouteError = require('./route_error');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
+const Audit = sequelize.define('Audit', {
+  event: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  key: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  token: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+const Authz = sequelize.define('Authorization', {
+  key: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  token: {
+    type: DataTypes.STRING,
+    unique: true
+  }
+}, {
+    tableName: 'Authz'
+});
+
+const Entity = sequelize.define('Entitiy', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+    tableName: 'Entities'
+});
+
+const RouteError = sequelize.define('RouteError', {
+  method: {
+    type: DataTypes.STRING
+  },
+  path: {
+    type: DataTypes.STRING
+  },
+  event: {
+    type: DataTypes.STRING
+  },
+  message: {
+    type: DataTypes.STRING
+  },
+  token: {
+    type: DataTypes.STRING
+  }
+});
+
+// Sync the models
+sequelize.sync({ force: true });
+
+// Export the models
 module.exports = {Entity, Authz, Audit, RouteError};
