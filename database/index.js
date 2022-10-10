01c9c263d59a2ad998651c11678e421163ad41eb
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-10 10:22:37 -0400

    Curries neo4j database connection

--- database/index.js
+++ database/index.js
@@ -1,4 +1,5 @@
 const local = require('./local');
 const postgres = require('./postgres');
+const neo4j = require('./neo4j');
 
-module.exports = { local, postgres };
+module.exports = { local, postgres, neo4j };
