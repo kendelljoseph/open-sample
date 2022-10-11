e37b1af110e22b2f985947b5e24e7f4a0fc4c96c
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-11 17:00:47 -0400

    Implements queue

--- middleware/audit.js
+++ middleware/audit.js
@@ -1,20 +1,28 @@
 import crypto from 'crypto';
 import { Audit } from '../models/record/index.js';
+import enqueue from './lib/enqueue.js';
 
 export default () => async (req, res, next) => {
   const appEvent = req.headers['x-app-audit-event'];
 
-  try {
-    const audit = await Audit.create({
-      token: req.authz && req.authz.token,
-      key: crypto.randomBytes(32).toString('hex'),
-      event: appEvent,
-    });
+  enqueue(
+    req.authz.token,
+    async () => {
+      try {
+        const audit = await Audit.create({
+          token: req.authz && req.authz.token,
+          key: crypto.randomBytes(32).toString('hex'),
+          event: appEvent,
+        });
 
-    req.audit = audit.dataValues;
+        req.audit = audit.dataValues;
 
-    next();
-  } catch (error) {
-    next(error);
-  }
+        next();
+      } catch (error) {
+        next(error);
+      }
+    },
+    next,
+    appEvent,
+  );
 };
