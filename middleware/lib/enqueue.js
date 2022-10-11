3ba9a65a162e172ea4af20cc02dd87896ad02f83
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-11 16:58:52 -0400

    Updates logging and formatting

--- middleware/lib/enqueue.js
+++ middleware/lib/enqueue.js
@@ -2,14 +2,14 @@
 import PQueue from 'p-queue';
 
 const queue = {};
-export default (token, fn, next) => {
+export default (token, fn, next, ...args) => {
   if (!queue[token]) queue[token] = new PQueue({ concurrency: 1 });
   // eslint-disable-next-line no-console
-  console.log(`🌺: ${Object.keys(queue).length}`.cyan);
+  console.log(`🌺${args.pop() || ''}: ${Object.keys(queue).length}`.cyan, args);
 
   queue[token].add(fn);
+  queue[token].on('error', (error) => next({ statusCode: 401, message: error }));
   queue[token].on('empty', () => {
     delete queue[token];
   });
-  queue[token].on('error', (error) => next({ statusCode: 401, message: error }));
 };
