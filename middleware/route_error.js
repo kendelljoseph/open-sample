8cc2c9e83014fd53fef4668055f1a8a3b697d38a
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-11 17:00:10 -0400

    Implements queue

--- middleware/route_error.js
+++ middleware/route_error.js
@@ -1,25 +1,36 @@
 import { RouteError } from '../models/record/index.js';
+import enqueue from './lib/enqueue.js';
 
 export default () => async (error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   // eslint-disable-next-line no-console
+  if (!req.authz) console.error('UNAUTHORIZED REQUEST'.red);
+  // eslint-disable-next-line no-console
   console.error(`${statusCode} Server Error`.bgRed, String(error.message).red);
-  try {
-    await RouteError.create({
-      method: req.method,
-      path: req.path,
-      event: req.audit && req.audit.event,
-      message: JSON.stringify(error.message),
-      statusCode,
-      token: req.authz && req.authz.token,
-    });
-    if (!res.writableEnded) {
-      res.status(statusCode).json(error.message);
-    }
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error('500 Cascading Database or Express Error'.bgRed, err);
-  }
 
-  next();
+  enqueue(
+    req.authz || req.authz.token,
+    async () => {
+      try {
+        await RouteError.create({
+          method: req.method,
+          path: req.path,
+          event: req.audit && req.audit.event,
+          message: JSON.stringify(error.message),
+          statusCode,
+          token: req.authz && req.authz.token,
+        });
+        if (!res.writableEnded) {
+          res.status(statusCode).json(error.message);
+        }
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error('500 Cascading Database or Express Error'.bgRed, err);
+      }
+
+      next();
+    },
+    next,
+    'route error',
+  );
 };
