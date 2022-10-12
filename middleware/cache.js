8f379420546245500829f373e0708390609c32cb
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-12 11:15:20 -0400

    Adds cache

---
+++ middleware/cache.js
@@ -0,0 +1,41 @@
+import NodeCache from 'node-cache';
+
+class Cache {
+  constructor(...args) {
+    this.cache = new NodeCache(...args);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  generateRequestKey(req) {
+    const appEvent = req.headers['x-app-audit-event'] || 'unknown-event';
+    return `${req.originalUrl}/${appEvent}&accessToken=${req.authz.token}`;
+  }
+
+  has(...args) {
+    return this.cache.has(...args);
+  }
+
+  set(...args) {
+    return this.cache.set(...args);
+  }
+
+  get(...args) {
+    // eslint-disable-next-line no-console
+    console.info('💾:cache'.cyan);
+    return this.cache.get(...args);
+  }
+
+  del(...args) {
+    // eslint-disable-next-line no-console
+    console.info('💾:cache:deleted'.red);
+    return this.cache.del(...args);
+  }
+}
+
+const localCache = new Cache({ stdTTL: 2, checkperiod: 2 });
+
+export default () => async (req, res, next) => {
+  req.localCache = localCache;
+
+  next();
+};
