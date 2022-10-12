ba746637e345f21ee40b7ec5f2e0eb9a36388bfc
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-11 22:54:24 -0400

    Implements queues for all routes

--- routes/audit.js
+++ routes/audit.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Audit } from '../models/record/index.js';
 import audit from '../middleware/audit.js';
+import enqueue from '../middleware/lib/enqueue.js';
 
 const router = express.Router();
 
@@ -8,30 +9,43 @@ router.use(audit());
 
 // Get All Audits
 router.get('/', async (req, res, next) => {
-  try {
-    const record = await Audit.findAll();
-    const records = record.map(({ dataValues }) => dataValues);
+  enqueue(
+    req.authz ? req.authz.token : 'unauthorized',
+    async () => {
+      try {
+        const record = await Audit.findAll();
+        const records = record.map(({ dataValues }) => dataValues);
 
-    res.status(200).json(records);
-  } catch (error) {
-    next(error);
-  }
+        res.status(200).json(records);
+      } catch (error) {
+        next(error);
+      }
+    },
+    next,
+    'admin:audit',
+  );
   return null;
 });
 
 // Get Audit by Event
 router.get('/:event', async (req, res, next) => {
   const { event } = req.params;
-
-  try {
-    const record = await Audit.findAll({
-      where: { event },
-    });
-    const records = record.map(({ dataValues }) => dataValues);
-    res.status(200).json(records);
-  } catch (error) {
-    next(error);
-  }
+  enqueue(
+    req.authz ? req.authz.token : 'unauthorized',
+    async () => {
+      try {
+        const record = await Audit.findAll({
+          where: { event },
+        });
+        const records = record.map(({ dataValues }) => dataValues);
+        res.status(200).json(records);
+      } catch (error) {
+        next(error);
+      }
+    },
+    next,
+    `admin:audit:${event}`,
+  );
   return null;
 });
 
--- routes/route_error.js
+++ routes/route_error.js
@@ -2,29 +2,45 @@ import express from 'express';
 import { RouteError } from '../models/record/index.js';
 
 import audit from '../middleware/audit.js';
+import enqueue from '../middleware/lib/enqueue.js';
 
 const router = express.Router();
 
 router.use(audit());
 
 // Get All RouteError
-router.get('/', async (req, res) => {
-  const error = await RouteError.findAll();
-  const records = error.map(({ dataValues }) => dataValues);
+router.get('/', async (req, res, next) => {
+  enqueue(
+    req.authz ? req.authz.token : 'unauthorized',
+    async () => {
+      const error = await RouteError.findAll();
+      const records = error.map(({ dataValues }) => dataValues);
 
-  res.status(200).json(records);
+      res.status(200).json(records);
+    },
+    next,
+    'admin:route error',
+  );
+
   return null;
 });
 
 // Get RouteError by Event
 router.get('/:event', async (req, res) => {
-  const { event } = req.params;
-  const error = await RouteError.findOne({
-    where: { event },
-  });
-  const record = error && error.dataValues;
+  enqueue(
+    req.authz ? req.authz.token : 'unauthorized',
+    async () => {
+      const { event } = req.params;
+      const error = await RouteError.findOne({
+        where: { event },
+      });
+      const record = error && error.dataValues;
 
-  res.status(200).json(record);
+      res.status(200).json(record);
+    },
+    next,
+    'admin:route error',
+  );
   return null;
 });
 
