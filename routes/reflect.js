cef2a6e882fb1a9c7eb99dc4ceab65d2a6a9aeab
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-14 15:07:21 -0400

    #save reflection saves prompt as an entity

--- routes/reflect.js
+++ routes/reflect.js
@@ -48,20 +48,41 @@ router.post('/:app', async (req, res, next) => {
   const smsUrl = `${req.headers['x-forwarded-proto'] || 'http'}://${
     req.headers.host
   }/api/v1/sms/send`;
-  const config = {
+  const entityUrl = `${req.headers['x-forwarded-proto'] || 'http'}://${
+    req.headers.host
+  }/api/v1/entity`;
+  const config = (eventName) => ({
     headers: {
       Authorization: `Bearer ${APP.REFLECT_ACCESS_TOKEN}`,
-      'x-app-audit-event': 'reflect-api-call',
+      'x-app-audit-event': eventName,
     },
-  };
+  });
 
+  let savePrompt = null;
   if (tagFound) {
     try {
-      await axios.post(smsUrl, { to: FROM, message: 'Okay' }, { ...config });
+      if (hashtags.includes('#save')) {
+        savePrompt = String(BODY).split(' ');
+        savePrompt.shift();
+        savePrompt = savePrompt.join('');
+
+        await axios.post(
+          entityUrl,
+          { name: FROM, prompt: savePrompt },
+          { ...config('reflect:tag-found-entity-create') },
+        );
+      }
+
+      await axios.post(
+        smsUrl,
+        { to: FROM, message: 'Okay' },
+        { ...config('reflect:tag-found-sms') },
+      );
     } catch (error) {
       res.end();
       return next(error);
     }
+
     res.end();
     return null;
   }
@@ -74,13 +95,13 @@ router.post('/:app', async (req, res, next) => {
       };
 
       try {
-        const { data } = await axios.post(aiUrl, payload, config);
+        const { data } = await axios.post(aiUrl, payload, config('reflect:prompt'));
         let smsPayload = {};
         if (data.response) {
           smsPayload = { to: FROM, message: data.response };
         } else {
           // eslint-disable-next-line no-console
-          console.error('reflect:api/prompt:no-response'.bgRed, data);
+          console.error('reflect:prompt:no-response'.bgRed, data);
           smsPayload = { to: FROM, message: '🤷 IDK' };
           await axios.post(smsUrl, smsPayload, { ...config });
           res.end();
