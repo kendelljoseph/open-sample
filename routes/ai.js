feb3d936481daa3f923aa2e4656b532c20f4c22b
Kendell Joseph <kendell@Kendells-MBP.attlocal.net>
2022-10-18 13:00:50 -0400

    Adds code route

--- routes/ai.js
+++ routes/ai.js
@@ -64,4 +64,54 @@ router.post('/prompt', async (req, res, next) => {
   return null;
 });
 
+router.post('/code', async (req, res, next) => {
+  const { body } = req;
+  const appEvent = req.appAuditEvent;
+
+  // Validation
+  const errors = isAi(body);
+  if (errors.length) {
+    return next({ statusCode: 400, message: errors });
+  }
+
+  enqueue(
+    req.authz ? req.authz.token : 'unauthorized',
+    async () => {
+      const payload = {
+        model: 'code-davinci-002',
+        prompt: body.prompt,
+        temperature: 0.8,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop: ['service:', 'user:', 'asker:'],
+      };
+
+      const url = APP.OPENAI_COMPLETIONS_URL;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${APP.OPENAI_API_KEY}`,
+        },
+      };
+
+      axios
+        .post(url, payload, config)
+        .then(({ data }) => {
+          res.json(
+            data.choices && data.choices.length
+              ? { prompt: req.body.prompt, response: data.choices[0].text }
+              : data,
+          );
+        })
+        .catch((error) => {
+          next(error);
+        });
+    },
+    next,
+    appEvent,
+  );
+  return null;
+});
+
 export default router;
